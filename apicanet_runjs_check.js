7edff29260dafb9d7e8613634e1161d682b610cd
Varvara Korol <varvara.korol@apicasystem.com>
2021-07-05 11:12:53 +0200

    Update apicanet_runjs_check.js

--- apicanet_runjs_check.js
+++ apicanet_runjs_check.js
@@ -1,7 +1,7 @@
 var start = Date.now() / 1000;
 var end = start + 1;
 var test = {
- "returncode": 1,
+ "returncode": 0,
  "stdout": "Javascript stdout",
  "stderr": "Javascript stderr",
  "start_time": start,
