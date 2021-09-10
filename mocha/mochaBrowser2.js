ebc2f05811bd9b9b1c5fd06f087cbe5cdba8b3b2
Pat Wakeem <patwakeem@gmail.com>
2021-09-10 10:03:56 +0200

    Create mochaBrowser2.js

---
+++ mocha/mochaBrowser2.js
@@ -0,0 +1,15 @@
+var expect = require("chai").expect;
+var puppeteer = require('puppeteer');
+
+describe("browser", function() {
+    it("browser", async function() {
+    let pages;
+    let page;
+    let browser;
+    browser = await puppeteer.connect({browserURL: "http://localhost:9222/json",timeout:5000});
+    pages = await browser.pages();
+    page = pages[0];
+	  page.goto("https://www.reddit.com").then(res => expect(1+1).to.equal(2));
+    await browser.close();
+   });
+});
