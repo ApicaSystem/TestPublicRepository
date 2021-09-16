ae28b6f5736d14136f2f928eec3f530b1ec3ba7d
Pat Wakeem <patwakeem@gmail.com>
2021-09-16 14:41:09 +0200

    Update test.js

--- puppeteerTests/test.js
+++ puppeteerTests/test.js
@@ -7,9 +7,7 @@ var pages;
     browser = await puppeteer.connect({browserURL: "http://localhost:9222/json", timeout:1000});
     pages = await browser.pages();
     page = pages[0];
-    await page.goto("https://www.apica.io");
     await page.goto("https://www.reddit.com");
-    await page.goto("https://www.example.com");
-    console.log("done")
-    process.exit(0);
+    await page.screenshot({ path: 'my_screenshot.png' })
+    await puppeteer.close();
 })()
