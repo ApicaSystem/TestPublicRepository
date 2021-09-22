c5fa08e1f062b1a37f23640b85964a0cc89d40be
Pat Wakeem <patwakeem@gmail.com>
2021-09-22 12:54:24 +0200

    Update mochaBrowser.js

--- mocha/mochaBrowser.js
+++ mocha/mochaBrowser.js
@@ -1,12 +1,17 @@
 var expect = require("chai").expect;
 var puppeteer = require('puppeteer');
+var options = {
+	executablePath: '/usr/lib/chromium/chrome',
+	headless: false,
+	args: ['--disable-gpu', '--no-sandbox', '--remote-debugging-port=9222', '--disable-dev-shm-usage']
+}
 
 describe("browser", function() {
     it("browser", async function() {
     let pages;
     let page;
     let browser;
-    browser = await puppeteer.connect({browserURL: "http://localhost:9222/json",timeout:5000});
+    browser = await puppeteer.launch(options);
     pages = await browser.pages();
     page = pages[0];
     await page.goto("https://www.example.com").then(res => expect(1+1).to.equal(2));
